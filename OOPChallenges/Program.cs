0c6f34465326487884f6bab64fa517c96f568f2c
Opilane TTHK <opilane@tthk.ee>
2020-11-26 13:18:07 +0200

    OOP

---
+++ OOPChallenges/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace OOPChallenges
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            double happiness;
+            int spots;
+
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                happiness = 0;
+                spots = _spots;
+                Console.WriteLine($"The dog {name} has been created");
+            }
+
+            public void Bark()
+            {
+                Console.WriteLine($"{name} barks!");
+
+                happiness += 0.2;
+            }
+
+            public void Wiggle()
+            {
+                Console.WriteLine($"{name} wiggles its tail!");
+            }
+
+            public double Happiness
+            {
+                get { return happiness; }
+               
+            }
+
+            public void Rename(string newName)
+            {
+                name = newName; 
+            }
+
+            public void ShowDogData()
+            {
+                Console.WriteLine($"Name: {name}; Spots: {spots}; Happiness: {happiness}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dog newDog = new Dog("KogMaw", 10);
+            Console.WriteLine($"The level of happiness: {newDog.Happiness}");
+
+            while(newDog.Happiness != 1)
+            {
+                newDog.Bark();
+            }
+
+            Console.WriteLine($"The level of happiness: {newDog.Happiness}");
+
+            newDog.Wiggle();
+
+            Console.WriteLine("Name the dog: ");
+            string newDogName = Console.ReadLine();
+            newDog.Rename(newDogName);
+
+            newDog.ShowDogData();
+
+           
+
+
+
+
+        }
+    }
+}
---
+++ WheresMyCar/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace WheresMyCar
+{
+    class Program
+    {
+        class Car
+        {
+            string name;
+            string modelName;
+            string regNum;
+            string color;
+            int odometer;
+            int fueltank;
+
+            public Car(string _name, string _modelName, string _regNum, string _color, int _odometer, int _fueltank)
+            {
+                name = _name;
+                modelName = _modelName;
+                regNum = _regNum;
+                color = _color;
+                odometer = 0;
+                fueltank = 60;
+
+            }
+
+            public void Drive()
+            {
+                Console.WriteLine($"The {name} {modelName} passes a lap");
+
+                odometer += 100;
+                fueltank -= 5;
+            }
+
+            public double Odometer
+            {
+                get { return odometer; }
+            }
+
+            public double Fueltank
+            {
+                get { return fueltank; }
+            }
+
+            public void ShowCarData()
+            {
+                Console.WriteLine($"Name: {name}, Model name: {modelName}, Registration number: {regNum}, Color: {color}, Current odometer state: {odometer}, Current fueltank state: {fueltank}");
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Car newCar = new Car("Toyota Celica", "XX 2800GT", "SOZ251", "Green", 0, 60);
+            Console.WriteLine($"Odometer: {newCar.Odometer} Fueltank: {newCar.Fueltank}");
+
+            while(newCar.Fueltank != 0)
+            {
+                newCar.Drive();
+
+                Console.WriteLine($"Odometer: {newCar.Odometer} Fueltank: {newCar.Fueltank}");
+            }
+
+
+            Console.WriteLine($"The car has stopped! You ran out of fuel");
+
+            newCar.ShowCarData();
+        }
+    }
+}
